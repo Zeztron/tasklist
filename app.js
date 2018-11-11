f8e5bf47931bf4e694c19716c2ea8f695579ecb0
Zeztron <pateltharsh@gmail.com>
2018-11-11 12:17:47 -0500

    Added delete and filter tasks functionality

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Define UI vars
 const form = document.querySelector("#task-form");
 const taskList = document.querySelector(".collection");
-const clearBtn = document.querySelector(".cleat-tasks");
+const clearBtn = document.querySelector(".clear-tasks");
 const filter = document.querySelector("#filter");
 const taskInput = document.querySelector("#task");
 
@@ -11,7 +11,14 @@ loadEventListeners();
 function loadEventListeners() {
     // Add task event
     form.addEventListener("submit", addTask);
+    // Remove task events
+    taskList.addEventListener("click", removeTask);
+    // Clear all tasks events
+    clearBtn.addEventListener("click", clearTasks);
+    // Filter tasks event
+    filter.addEventListener("keyup", filterTasks);
 }
+
 
 // Add task
 function addTask(e) {
@@ -29,7 +36,7 @@ function addTask(e) {
     // Add class
     link.className = "delete-item secondary-content";
     // Add icon HTML
-    link.innerHTML = '<i class="fa fa-remove"></i>';
+    link.innerHTML = '<i class="fa fa-times" style="cursor: pointer"></i>';
     // Append the link to the li
     li.appendChild(link);
     // Append li to ul
@@ -38,5 +45,46 @@ function addTask(e) {
     taskInput.value = "";
     e.preventDefault();
 }
+
+// Remove task
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains("delete-item")) {
+        e.target.parentElement.parentElement.remove();
+    }
+}
+
+// Clear tasks
+function clearTasks(e) {
+    if (confirm("Are you sure?")) {
+        while(taskList.firstChild) {
+            taskList.removeChild(taskList.firstChild);
+        }
+    }
+}
+
+// Filter Task
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll(".collection-item").forEach(function(task) {
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = "block";
+        } else {
+            task.style.display = "none";
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
